f2a9d42e75cce3e790f8fb8077a4b7a1dbf279a6
danimen81 <d4nimen@gmail.com>
2023-05-31 22:52:46 -0400

    esto

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.export = {
+  mode: 'development',
+  entry: './src/main.js',
+  output:{
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules:  [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use:  {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
